df88d89598f69ec552d03a9ca4b2244bd17599e1
dropleaves <mobingsen@xiao100.com>
2021-05-26 11:02:31 +0800

    hdp import

--- hdp/src/test/java/com/drop/leaves/hdp/hdfs/TestHdfs.java
+++ hdp/src/test/java/com/drop/leaves/hdp/hdfs/TestHdfs.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
