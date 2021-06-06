f81ce9265dafa838ad5fda54b42561a6904b5a86
2740303622@qq.com <mobingsen>
2021-06-06 23:58:18 +0800

    mybatis

---
+++ hdp/src/main/java/com/drop/leaves/hdp/module/fof/FofReduce.java
@@ -0,0 +1,9 @@
+package com.drop.leaves.hdp.module.fof;
+
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ * @author by mobingsen on 2021/6/5 14:14
+ */
+public class FofReduce extends Reducer {
+}
