d890285f772076a67e60b780eac72d19abe6a685
2740303622@qq.com <mobingsen>
2021-06-07 23:42:14 +0800

    mybatis

---
+++ mybatis/src/main/java/com/drop/leaves/mybatis/MybatisApplication.java
@@ -0,0 +1,15 @@
+package com.drop.leaves.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author by mobingsen on 2021/6/7 23:34
+ */
+@SpringBootApplication
+public class MybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class);
+    }
+}
---
+++ mybatis/src/test/java/com/drop/leaves/mybatis/MybatisApplicationTest.java
@@ -0,0 +1,7 @@
+package com.drop.leaves.mybatis;
+
+/**
+ * @author by mobingsen on 2021/6/7 23:35
+ */
+public class MybatisApplicationTest {
+}
