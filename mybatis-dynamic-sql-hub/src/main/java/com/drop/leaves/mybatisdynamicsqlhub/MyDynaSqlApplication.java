df7ed15121eeac4762b456ad4125402c3595403d
2740303622@qq.com <mobingsen>
2021-06-16 00:55:51 +0800

    mybatis dynamic sql

--- mybatis-dynamic-sql-hub/src/main/java/com/drop/leaves/mybatisdynamicsqlhub/MyDynaSqlApplication.java
+++ mybatis-dynamic-sql-hub/src/main/java/com/drop/leaves/mybatisdynamicsqlhub/MyDynaSqlApplication.java
@@ -1,12 +1,13 @@
 package com.drop.leaves.mybatisdynamicsqlhub;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author by mobingsen on 2021/6/10 20:51
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 public class MyDynaSqlApplication {
 
     public static void main(String[] args) {
